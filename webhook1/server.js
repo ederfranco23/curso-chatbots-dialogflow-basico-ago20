2e6f900d62222160a42a8dd46a32f5e1f731b8ab
Eder Martins Franco <eder.franco@fpf.br>
2020-08-26 19:49:30 -0400

    WIP

---
+++ webhook1/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios")
+const app = express();
+app.use(bodyParser.json());
+
+
+app.get("/webhook", (request, response) => {
+  response.json({ fulfillmentText: "CHEGOU NA API."});
+});
+
+
+app.post("/webhook", (request, response) => {
+  console.log("request.body", request.body);
+  const intent = request.body.queryResult.intent.displayName;
+  
+  if(intent === 'login') {
+      const { email, password } = request.body.queryResult.parameters;
+      //response.json({ fulfillmentText: `Logando com ${email} e ${password}` });
+      
+      axios.post("https://reqres.in/api/login", {email, password}).then(apiRes => {
+          console.log(apiRes);
+          response.json({ fulfillmentText: `Deu certo! Você está logado! (${apiRes.data.token})` });
+      }).catch(error => {
+          response.json({ fulfillmentText: 'Dados de acesso inválidos.' });
+      });
+  } else {
+      response.json({ fulfillmentText: 'Poxa! Não sei como te ajudar...' });
+  }
+
+});
+
+// listen for requests :)
+const listener = app.listen(8888, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
